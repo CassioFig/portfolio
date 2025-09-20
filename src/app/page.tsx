bcf74e371f28700127c7131416d06acbbf2f679d
Cassio Figueiredo <cassio.figueiredof@gmail.com>
2025-09-19 23:41:13 -0700

    style: Update icon color in skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
               return (
                 <div key={skill.id} className="border-dark border-2 rounded-lg px-6 py-6">
                   <div className='flex items-center gap-4'>
-                    <Icon style={{ color: 'var(--color-green-3)' }} />
+                    <Icon style={{ color: 'var(--color-green-1)' }} />
                     <h3 className="text-2xl font-semibold text-white">{ t.pages.home.skills[skill.id as keyof typeof t.pages.home.skills] }</h3>
                   </div>
                   <ul className="flex flex-wrap mt-4 gap-2">
